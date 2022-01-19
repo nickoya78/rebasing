b4b5ef20a03f441b55cfc6d0c5dd14b892217d2c
Nicholas Oyagh <you@example.com>
2022-01-18 18:12:10 -0800

    files added

---
+++ rebasing.js
@@ -0,0 +1,11 @@
+let mike = "mike";
+let john = "john";
+let sarah = "sarah";
+let linda = "linda";
+let family = 1989;
+if (mike == 1990) {
+  console.log("young adult");
+} else {
+  console.log("how old are you?");
+}
+let people = [1990, 1992, 1994, 1996]; //main branch
