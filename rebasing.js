5640fdfdcac7275ec07cffb591fc262585bcac92
Nicholas Oyagh <you@example.com>
2022-01-18 18:37:27 -0800

    updated

--- rebasing.js
+++ rebasing.js
@@ -1,11 +1,11 @@
-let mike = "mike";
-let john = "john";
-let sarah = "sarah";
-let linda = "linda";
-let family = 1989;
+const mike = "mike";
+const john = "john";
+const sarah = "sarah";
+const linda = "linda";
+const family = 1989;
 if (mike == 1990) {
   console.log("young adult");
 } else {
   console.log("how old are you?");
 }
-let people = ["MIK", "JOH", "SARH", "LIN"]; //film branch
+const people = ["MIK", "JOH", "SARH", "LIN"]; //film branch
